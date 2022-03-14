afe1dfa57225b8dc91e393125daa7dbdb0ec12b9
BryanLicea <bryanwi2002@gmail.com>
2022-03-14 17:42:18 -0600

    Bollards y laser ya controla cortina
    +Mejoras en el mapa(muros)

--- Cortina/Cortina.gd
+++ Cortina/Cortina.gd
@@ -2,8 +2,8 @@ extends StaticBody
 
 
 var down_vel= 3
-var up_vel = 4
-var bajar = false
+var up_vel = 7
+
 var pos = 7
 
 var state = 0
@@ -20,23 +20,18 @@ func _ready():
 func _process(delta):
 	
 	if state==-1:
-		mover(up_vel, delta)
+		mover(-up_vel, delta)
 	elif state == 0:
 		pass #return
 	elif state ==1:
 		mover(down_vel,delta)
+	
 	if Input.is_action_pressed("ui_down"):
-		mover(down_vel,delta)
+		state = 1
 	elif Input.is_action_pressed("ui_up"):
-		mover(-up_vel,delta)
+		state = -1
 	elif Input.is_action_just_pressed("ui_left"):
-		bajar = false
-		trasladar(3.5)
-		
-	elif Input.is_action_just_pressed("ui_down"):
-		bajar = true
-	if bajar==true:
-		mover(down_vel,delta)
+		state = 0
 	
 
 
@@ -62,3 +57,6 @@ func trasladar(var size:float):
 	self.scale = Vector3(1,y_scale,1)
 	self.translation = Vector3(0,7-size,0)
 	pass
+
+func changeState (stat:int):
+	state = stat
--- sensores/Laser.gd
+++ sensores/Laser.gd
@@ -4,15 +4,16 @@ onready var raycast = $RayCast
 onready var laser_mask = $CSGCombiner/CSGBox
 onready var cylinder = $CSGCombiner/CSGCylinder
 
-#export var lenght = 10
+export var lenght = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_as_toplevel(true)
 	pass
-	#cylinder.height = lenght
-	#cylinder.translation = Vector3(0,lenght/2+0.5,0)
-	#laser_mask.translation = Vector3(0,lenght,0)
-	#raycast.cast_to = Vector3(0,lenght,0)
+	cylinder.height = lenght
+	cylinder.translation = Vector3(0,lenght/2+0.5,0)
+	laser_mask.translation = Vector3(0,lenght,0)
+	raycast.cast_to = Vector3(0,lenght,0)
 
 
 
@@ -23,5 +24,11 @@ func _process(delta):
 	
 	if object != null:
 		laser_mask.height = hitpoint.distance_to(laser_mask.global_transform.origin) *2
+		
 	else:
 		laser_mask.height = 0.01
+	
+	if  object !=null and !object.is_in_group("Ignore"):
+		get_parent().changeState(-1)
+	else:
+		return
