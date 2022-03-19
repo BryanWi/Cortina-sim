efee8d3584e19beb7e20e73ec85aceaca2685138
BryanLicea <bryanwi2002@gmail.com>
2022-03-18 20:37:26 -0600

    Ajuste de grados maximos para cámara

--- character/SpringArm.gd
+++ character/SpringArm.gd
@@ -11,7 +11,7 @@ func _ready():
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		rotation_degrees.x = clamp(rotation_degrees.x, -80.0, 30.0)
+		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 90.0)
 		
 		
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
